ac7abc7e2865d6f4b35d6e77669f564b294a7e87
Alejandrodelosrios <alejandrodelosrios09@gmail.com>
2026-06-05 17:45:41 -0400

    feat: configuracion inicial del backend

---
+++ src/main/java/com/vitalbite/documental/VitalbiteDocumentalApplication.java
@@ -0,0 +1,13 @@
+package com.vitalbite.documental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VitalbiteDocumentalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VitalbiteDocumentalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vitalbite/documental/VitalbiteDocumentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitalbite.documental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VitalbiteDocumentalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
